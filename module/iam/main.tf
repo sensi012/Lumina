c7ba1a878acc4aa8ce50e71ee6a3f71bc1c8ddc8
Isaiah <isaiahadepegba@gmail.com>
2026-08-11 00:07:17 +0100

    fix: update iam trust policy to be case-insensitive for github repo name

--- module/iam/main.tf
+++ module/iam/main.tf
@@ -24,7 +24,10 @@ data "aws_iam_policy_document" "github_assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_org}/${var.github_repo}:*"]
+      values   = [
+        "repo:${var.github_org}/Lumina:*",
+        "repo:${var.github_org}/lumina:*"
+      ]
     }
   }
 }
