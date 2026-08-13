02e8950339769947c1ece8480da9356032c7f852
Isaiah <isaiahadepegba@gmail.com>
2026-08-13 14:11:11 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket       = "lumina-terraform-state"
-#     key          = "lumina/static-site/terraform.tfstate"
-#     region       = "us-east-1"
-#     encrypt      = true
-#     use_lockfile = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket       = "lumina-terraform-state"
+    key          = "lumina/static-site/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
