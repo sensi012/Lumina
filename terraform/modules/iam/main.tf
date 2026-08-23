d80a9a1b12494cf3a2a56ecba23f192e3a233ad3
Isaiah <isaiahadepegba@gmail.com>
2026-08-23 23:21:40 +0100

    ci: scope OIDC role trust policy and deploy job to prod environment

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -3,8 +3,8 @@ data "tls_certificate" "github" {
 }
 
 resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
   thumbprint_list = [
     "6938fd4d98bab03faadb97b34396831e3780aea1",
     "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
@@ -27,10 +27,10 @@ data "aws_iam_policy_document" "github_assume_role" {
       values   = ["sts.amazonaws.com"]
     }
     condition {
-      test     = "StringLike"
+      test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = [
-        "repo:${var.github_org}/${var.github_repo}:*"
+      values = [
+        "repo:${var.github_org}/${var.github_repo}:environment:${var.environment}"
       ]
     }
   }
