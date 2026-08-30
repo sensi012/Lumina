d7b2b5b7f9b4b276af020359428f41e81f3cb360
Isaiah <isaiahadepegba@gmail.com>
2026-08-30 01:33:06 +0100

    fix(iam/cd): add s3:GetObject for backend lockfile and use workspace -or-create

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -46,9 +46,11 @@ data "aws_iam_policy_document" "deploy_policy" {
   statement {
     effect = "Allow"
     actions = [
+      "s3:GetObject",
       "s3:PutObject",
       "s3:DeleteObject",
-      "s3:ListBucket"
+      "s3:ListBucket",
+      "s3:GetBucketLocation"
     ]
     resources = [
       var.s3_bucket_arn,
